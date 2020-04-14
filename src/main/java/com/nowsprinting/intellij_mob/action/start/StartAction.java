76a0e961778b89ef5ebd1aa5b1fa8c0cd3ad2bed
Koji Hasegawa <hasegawa@hubsys.co.jp>
2020-04-15 06:34:43 +0900

    Move timer service

--- src/main/java/com/nowsprinting/intellij_mob/action/start/StartAction.java
+++ src/main/java/com/nowsprinting/intellij_mob/action/start/StartAction.java
@@ -3,7 +3,7 @@ package com.nowsprinting.intellij_mob.action.start;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.nowsprinting.intellij_mob.MobBundle;
-import com.nowsprinting.intellij_mob.MobTimer;
+import com.nowsprinting.intellij_mob.service.TimerService;
 import com.nowsprinting.intellij_mob.action.start.ui.StartDialog;
 import com.nowsprinting.intellij_mob.config.MobProjectSettings;
 import org.jetbrains.annotations.NotNull;
@@ -12,7 +12,7 @@ public class StartAction extends AnAction {
 
     @Override
     public void update(@NotNull AnActionEvent e) {
-        MobTimer timer = MobTimer.getInstance(e.getProject());
+        TimerService timer = TimerService.getInstance(e.getProject());
         e.getPresentation().setEnabled(!timer.isRunning());
     }
 
--- src/main/java/com/nowsprinting/intellij_mob/service/TimerService.java
+++ src/main/java/com/nowsprinting/intellij_mob/service/TimerService.java
@@ -1,16 +1,17 @@
-package com.nowsprinting.intellij_mob;
+package com.nowsprinting.intellij_mob.service;
 
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
+import com.sun.istack.NotNull;
 
 import java.util.Date;
 
-public class MobTimer {
+public class TimerService {
     private Date startTime;
     private Date expireTime;
 
-    public static MobTimer getInstance(Project project) {
-        return ServiceManager.getService(project, MobTimer.class);
+    public static TimerService getInstance(@NotNull Project project) {
+        return ServiceManager.getService(project, TimerService.class);
     }
 
     public boolean isRunning() {
