bcc837c9662fcf2daaaf781c4ddec1abef31a199
Marcus Fihlon <marcus@fihlon.swiss>
2021-10-30 11:21:53 +0200

    :arrow_up: Upgrade mockk to v1.12.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
     implementation("com.googlecode.soundlibs:jlayer:1.0.1.4")
-    testImplementation("io.mockk:mockk:1.9")
+    testImplementation("io.mockk:mockk:1.12.0")
     testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.2")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.6.2")
 }
